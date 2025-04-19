a58a608759cf48b8176da04748a34f261cceacfe
moh9001 <mo9001@gmail.com>
2025-04-19 19:29:36 +0300

    Fix: move 127 exit to child process for command not found handling

--- shell.c
+++ shell.c
@@ -18,25 +18,25 @@ void execute_command(char *line)
 	if (argv == NULL)
 		return;
 
-	full_path = find_command(argv[0]);
-	if (!full_path)
-	{
-		perror("./hsh");
-		free(argv);
-		return;
-	}
-
 	pid = fork();
 	if (pid == -1)
 	{
 		perror("fork failed");
 		free(argv);
-		free(full_path);
 		exit(EXIT_FAILURE);
 	}
 
 	if (pid == 0)
 	{
+		/* Child process */
+		full_path = find_command(argv[0]);
+		if (!full_path)
+		{
+			dprintf(STDERR_FILENO, "./hsh: 1: %s: not found\n", argv[0]);
+			free(argv);
+			exit(127);
+		}
+
 		if (execve(full_path, argv, environ) == -1)
 		{
 			perror("./hsh");
@@ -47,6 +47,7 @@ void execute_command(char *line)
 	}
 	else
 	{
+		/* Parent process */
 		waitpid(pid, &status, 0);
 	}
 
